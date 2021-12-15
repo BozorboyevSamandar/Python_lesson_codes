c16e674d2a71fa24e6a24865a1a51dfb7ca3983b
SamandarBozorboyev <92523813+bscoders@users.noreply.github.com>
2021-12-15 12:19:36 +0500

    add 2.1 lesson
    2 dars kodlari

---
+++ 2.1-dars.py
@@ -0,0 +1,48 @@
+#---Python Kirish darsi N:2-----
+#----O'zgaruvchilar va ularga ustida ammallar bajarish------
+
+# ozgaruvchi = 'Salom'
+# son = 23
+# print(ozgaruvchi)
+# print(son)
+# print(ozgaruvchi, son)
+
+# a = "Assalomu Alaykum" # a degan o'zgaruvchiga "" ichida qiymat berdik
+# b = "Sarvar"
+# #print(a,b)
+# c = a + b
+# print(c)
+
+# a,c,b="Olma","Anor","Gilos"
+# print(a)
+# print(c)
+# print(b)
+
+
+
+ism = "Sarvar"
+yoshi = 19
+manzil = "Toshkent"
+print("Uning ismi" + ism + " yoshi" , yoshi , "da " + "U " + manzil + "da yashaydi.")
+
+
+bugun = 'Bugun\tkun\nissiq va \nyoqimli'
+print(bugun)
+
+# bugun = 'Bugun kun 
+# issiq va           #bu usul xato
+# yoqimli'
+# print(bugun)
+
+bugun = """Bugun kun
+issiq va 
+yoqimli"""
+print(bugun)
+
+bugun = '''Bugun kun
+issiq va 
+yoqimli'''
+print(bugun)
+
+matn = "Go'zal"
+
