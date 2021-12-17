0308b2cd634a11e9eac5c66d81113ec19f94b8cd
SamandarBozorboyev <92523813+bscoders@users.noreply.github.com>
2021-12-17 10:27:17 +0500

    add lesson 2.2
    2-dars kodlari

---
+++ 2.2-dars.py
@@ -0,0 +1,65 @@
+#---Python Kirish darsi N:2.2 -----
+#----O'zgaruvchilar va ularga ustida ammallar bajarish------
+
+
+
+# a = 'Salom '
+# print(a*5, " men dasturchiman")
+
+
+# a = "Bu bir matn, "  #so'zi ko'paytish
+# print(a*4)
+
+
+# a = c = b = "Hello" #bita qiymatni bir neshta o'zgaruvchiga berish
+# print(c)
+
+
+
+# a,b,c = "Bir","Ikki","Uch"
+# # print(a)
+# # print(b)   #misol uchun
+# # print(c)
+# print(a,b,c)
+
+
+
+
+# matn = "Bu uzun bir matn"
+# print(len(matn))            #matni neshta elementdan tashkil topganligini aniqlash
+
+
+# a = "Assalomu alaykum"
+# print(len(a))
+
+
+
+
+
+#matndan kerakli joyini qirqib olish
+#soz = "Gulxumor"
+# print(soz[3:7])  #index bo'yicha murojat qilish
+# print(soz[3:])
+# print(soz[1:3])
+# print(soz[-1])
+# print(soz[::-1])
+
+
+
+
+
+#o = soz[3:7]   #boshqa ozgaruvchiga xumo sozini biriktirish
+# print(o)
+
+
+# a = soz[0:3]
+# print(a)
+
+
+
+
+text = "assalomu aleykum"
+print(text.upper())
+print(text.lower())
+print(text.title())
+print(text.capitalize())
