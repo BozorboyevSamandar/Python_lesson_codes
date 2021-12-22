d1cdc0b94b8deed3f5c319379ddce99ec3cc8892
SamandarBozorboyev <92523813+bscoders@users.noreply.github.com>
2021-12-22 13:25:41 +0500

    add 3 lesson

---
+++ 3-dars.py
@@ -0,0 +1,23 @@
+#-- Python darsi N:3 -----
+#-- Malumotlar turlari (Data types) ------
+#-- Mentor: Samandar Bozorboyev ---
+
+#-------Ma'lumotlar Turlari------- Data types
+
+#matn-string:                str                   bunga misol -->     a = "ism"
+#raqamlar-intejer:           int                   bunga misol -->     b = 7
+#kasr sonlar:                float                 bunga misol -->     son = 7.33333
+#murakkab sonlar:            complex               bunga misol -->     son = 7j
+#boolean:                    true/false            bunga misol -->     19=="19"
+
+
+matn = 'salom'
+print(type(matn))
+son = 12
+print(type(son))
+son2 = 12.0
+print(type(son2))
+son3 = 7j
+print(type(son3))
+son4 = 20=='20'
+print(type(son4))
