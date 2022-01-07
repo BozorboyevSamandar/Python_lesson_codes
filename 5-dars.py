13149a4197de6ceff0e69668ce1ef11401c23e17
SamandarBozorboyev <92523813+bscoders@users.noreply.github.com>
2022-01-07 13:12:02 +0500

    add lesson 5

---
+++ 5-dars.py
@@ -0,0 +1,31 @@
+#-- Python darsi N:5 -----
+#-- Taqqoslash operatorlar -----
+#-- Mentor: Samandar Bozorboyev ---
+
+
+
+
+
+#                           TAQQOSLASH OPERATORLARI
+
+#Operator-----------------------------------------Ma'nosi-----------------------------------------Namuna
+#   >             agar chapdagi operand o'ngdagisidan katta bo'lsa True qaytaradi                 x > y
+#   <             agar chapdagi operand o'ngdagisidan kichik bo'lsa True qaytaradi                x < y
+#  ==             agar ikkita operand teng bo'lsa True qaytaradi                                  x == y
+#  !=             agar ikkita opernad teng bo'lmasa Ture qaytaradi                                x != y
+#  >=             katta yoki teng                                                                 x >= y
+#  <=             kichik yoki teng  
+# 
+
+
+#Namuna uchun misollar
+
+x = 10
+y = 5
+
+print("x>y: ", x>y)
+print("x<y: ", x<y)
+print("x==y: ", x==y)
+print("x!=y: ", x!=y)
+print("x>=y: ", x>=y)
+print("x>=y: ", x<=y)
