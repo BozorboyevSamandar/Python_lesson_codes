e3a00e2351031b74193700dce8f319ab9e78bc4d
SamandarBozorboyev <92523813+bscoders@users.noreply.github.com>
2022-01-07 12:56:59 +0500

    add lesson 4

---
+++ 4-dars.py
@@ -0,0 +1,54 @@
+#-- Python darsi N:4 -----
+#-- Arifmetik operatorlar, sonlar bilan ishlash -----
+#-- Mentor: Samandar Bozorboyev ---
+
+
+
+
+
+
+#                           ARIFMETIK OPERATORLAR
+#   operator                           ta'rif                                               namuna
+
+#-->   +     2ta operatorni qo'shadi yoki ishorani musbatga o'zgartiradi                      x+y+2
+#-->   -     O'ngdagi operandni chapdaginisidan ayiradi yoki ishorani manfiyga o'zgartiradi   x-y-2
+#-->   *     Ikkita operandni ko'paytiradi                                                    x*y
+#-->   /     Chapdagi operandni o'ngdagisiga bo'ladi(natija doim kasr ko'rinishida bo'ldi)    x/y
+#-->   %     Sonning moduli - Chapdagi operandning o'ngdagiga bo'lingandagi qoldig'i          x%y(x/y ning qoldig'i)
+#-->   //    Butun bo'linmaning butun qismini chiqaradi                                       x//y
+#-->   **    Darajaga oshirish - Chap operand ong operandga oshiriladi                        x**y(x ning y inchi darajasi)
+
+
+
+#Namuna uchun misollar
+
+# x = 16
+# y = 3
+# print("Natija: x+y >>",x+y)
+# print("Natija: x-y >>",x-y)
+# print("Natija: x*y >>",x*y)
+# print("Natija: x/y >>",x/y)
+# print("Natija: x%y >>",x%y)
+# print("Natija: x//y >>",x//y)
+# print("Natija: x**y >>",x**y)
+
+# a = 5
+# a = 5 + 5
+# a = a + 5
+# a += 5 #qo'shish, a ning qiymatiga qiymat qo'shdik
+# a -= 5 #ayirish
+# a /= 5 #bo'lish
+# a *= 5 #ko'paytirish va hokazo..
+# print(a)
+
+
+non_narxi = 2500
+non_dona = 4
+qiymat = non_narxi*non_dona
+print(non_dona,"ta non ",qiymat," so'm turadi")
+
+
+
+
+
+
