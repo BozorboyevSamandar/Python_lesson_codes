5929dce5b7cbc1b025ae6c82c4984920228bdaa3
SamandarBozorboyev <92523813+bscoders@users.noreply.github.com>
2022-03-22 15:25:39 +0500

    add lesson 6

---
+++ 6-dars.py
@@ -0,0 +1,30 @@
+#-- Python darsi N:6 -----
+#-- A’zolik(tegishlik) operatori, .format() funksiyasi va f “” string haqida -----
+#-- Mentor: Samandar Bozorboyev ---
+
+
+# # in - a'zolik operatori
+
+# a = 'Hello'
+# print("Hello" in a )
+
+# b = "Salom"
+# print("Salom" in b)
+
+# matn = "Assalomu Alaykum Anvar"
+# print("Anvar" in matn)
+
+# # not in - tegishlik emas
+
+# test = "Bugun havo"
+# print("issiq" not in test)
+
+
+# f "{}" string 
+ism  = 'Sirojiddin'
+yoshi = 18
+manzil = 'Toshkent'
+print(f"Uning ismi {ism} Yoshi esa {yoshi}da U {manzil}da yashaydi")
+
+#format funksiyasi
+print("Uning ismi {} Yoshi esa {}da U {}da yashaydi".format(ism,yoshi,manzil))
