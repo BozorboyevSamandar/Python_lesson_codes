a873afbfd0c059f3c57e11cf69eea6f8c0fe7750
BS_CODER <92523813+bscoders@users.noreply.github.com>
2022-04-18 00:54:27 -0700

    add 8 lesson

---
+++ 8-dars.py
@@ -0,0 +1,15 @@
+#-- Python darsi N: 8 ------
+#-- Foydalanuvchidan kiruvchi ma'lumotni olish -----
+#-- Mentor: Samandar Bozorboyev ---
+
+
+# ism = input("Ismingizni kiriting:")
+# fam = input("Familiyangizni kiriting:")
+# print("Assalomu alaykum hurmatli, " , ism, fam)
+
+
+# a  =  int(input("Birinchi soni kiriting: "))
+# b  =  int(input("Ikkinchi soni kiriting: "))
+# print("Sonlar yig'indisi", a+b)
+# print("Sonlar ayirmasi", a-b)
+# print("Sonlar ko'paytmasi", a*b)
