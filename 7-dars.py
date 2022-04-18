95201ccde918398349737bc3b8df350498b58f7a
BS_CODER <92523813+bscoders@users.noreply.github.com>
2022-04-18 00:54:04 -0700

    add 7 lesson

---
+++ 7-dars.py
@@ -0,0 +1,24 @@
+#-- Python darsi N: 7 ------
+#-- Mantiqiy operatorlar, and operatori uchun rostlik jadvali -----
+#-- Mentor: Samandar Bozorboyev ---
+
+#       MANTIQIY OPERATORLAR
+
+#--- OPERRATOR--------------MA'NOSI------------------NAMUNA
+#     and      agar ikkita operator rost bo'lsa        x and y
+#     or       agar operandlardan biri rost bo'lsa     x and y
+#     not      agar operand yolg'on bo'lsa True        not x
+
+
+#and misol 
+print(1==1 and 3==3)
+print(10 > 12 and 12 == 12)
+
+# or misol
+print(10==3 or 7+3==10) 
+print('salom' == 'salom' or 'world' == 'WorlD')
+print(4>5 or 21 == 22)
+
+# not misol
+print(not 1==1)
+print(not 1!=1)
