336ef2c88ae2e3fea2e24de4e5590153d6d78780
BS_CODER <92523813+bscoders@users.noreply.github.com>
2022-04-29 22:02:14 -0700

    add lesson 13

---
+++ 13-dars.py
@@ -0,0 +1,53 @@
+#-- Python darsi N: 12 ------
+#-- 13. Ro’yxat bilan ishlash (Sets) -----
+#-- Mentor: Samandar Bozorboyev ---
+
+# List - Ro'yxat
+# Tuple - Qator
+# Set - To'plam
+# Dictionary - Lug'at
+
+# {}
+
+# my_set = {'apple', 'banan', 'cherry'}
+# print(my_set)
+
+# set1 = {'apple', 'banan', 'cherry'}
+# set2 = {1, 45, 21, 7, 9}
+# set3 = {True, False, False}
+
+# print(set1)
+# print(set2)
+# print(set3)
+
+# my_set = {'abc', 32, True, 17, 'string', 32.4, True}
+# print(my_set)
+# print(type(my_set))
+# print(len(my_set))
+
+# to'plamga element qo'shish
+# thisset = {'apple', 'banan', 'cherry'}
+# thisset.add('orange')
+# print(thisset)
+
+# update() - elementlarni boshqa to'plamdan o'ziga qo'shib olish
+# thisset = {'apple', 'banan', 'cherry'}
+# myset = {'mongo', 'kivi', 'limon'}
+# thisset.update(myset)
+# print(thisset)
+
+# # remove() - elementlarni o'chirish
+# thisset = {'apple', 'banan', 'cherry'}
+# thisset.remove('banan')
+# print(thisset)
+
+# # discard() - elementni o'chirish
+# thisset.discard('apple')
+# print(thisset)
+
+
+# thisset = {'apple', 'banan', 'cherry'}
+# # thisset.pop() ## pop() - oxirgi elementni o'chiradi
+# # thisset.clear() ## clear() - to'plamni tozalaydi
+# del thisset
+# print(thisset)
