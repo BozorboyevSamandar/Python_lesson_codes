a24fe64f95d19e08ec4331406588e504bcd59f82
BS_CODER <92523813+bscoders@users.noreply.github.com>
2022-05-05 22:13:34 -0700

    add 15 lesson

---
+++ 15-dars.py
@@ -0,0 +1,36 @@
+# if elif else
+
+
+# if operatorining sintaksisi
+# if shart ifda:
+#     byuruq(s)
+
+# a = 44
+# b = 50
+# if b < a:
+#     print(b, ' ', a, 'dan katta')
+    
+
+# a = 34
+# b = 33
+# if a > b:
+#     print(a, 'katta qiymat')
+# elif a < b:
+#     print(b, 'katta qiymat')
+# else:
+#     print('Ikkala qiymat teng')
+
+# # Bir qator if bayonoti:
+# if a > b: print('a qiymat katta')
+
+
+
+a = 20
+b = 18
+c = 21
+if a > b and a > c:
+    print(a)
+elif b > a and b > c:
+    print(b)
+else:
+    print(c)
