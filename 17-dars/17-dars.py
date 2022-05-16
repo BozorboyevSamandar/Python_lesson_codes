745cf1f9d2e35a5efad68f199cb7a99b9434e43e
BS_CODER <92523813+BozorboyevSamandar@users.noreply.github.com>
2022-05-16 10:45:47 +0500

    add lesson 17

---
+++ 17-dars/17-dars.py
@@ -0,0 +1,55 @@
+#-- Python darsi N: 17 ------
+#-- 17. Ma’lumotlar oqimini boshqarish. Sikllar(For loops)  -----
+#-- Mentor: Samandar Bozorboyev ---
+
+
+# for sikli ketma-ketlikni takrorlash uchun ishlatiladi(ya'ni ro'yxat, to'plam, qator, lug'at)
+
+## for orqali listdagi qiymatlarni chiqardik
+# fruits = ['apple', 'banana', 'cherry']
+# for x in fruits:
+#     print(x)
+
+
+## for 
+# for text in 'Salom':
+#     print(text)
+
+## break - kerakli joyda shartni to'xtadadi
+# fruits = ['apple', 'banana', 'cherry']
+# for x in fruits:
+#     print(x)
+#     if x == 'banana':
+#         break
+
+## break 
+# fruits = ['apple', 'banana', 'cherry']
+# for x in fruits:
+#     if x == 'banana':
+#         break
+#     print(x)
+
+## continue - berilgan qiymatni chop qilmasdan ketadi xatolik bor joylarda xatoni tashlab ketadi
+# fruits = ['apple', 'banana', 'cherry']
+# for x in fruits:
+#     if x == 'banana':
+#         continue
+#     print(x)
+
+#range()
+
+# for x in range(8):
+#     print(x)
+
+
+# for x in range(2, 8):
+#     print(x)
+
+
+# for x in range(2, 30, 3):
+#     print(x)
+
+    
+# for x in range(8):
+#     if x == 5: break
+#     print(x)
