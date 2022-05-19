42f8ce5cdc4000ea5fa720bbb76a311af15ea05e
BS_CODER <92523813+BozorboyevSamandar@users.noreply.github.com>
2022-05-19 10:41:07 +0500

    add lesson 18

---
+++ 18-dars/18-dars.py
@@ -0,0 +1,34 @@
+#-- Python darsi N: 17 ------
+#-- 18. Ma’lumotlar oqimini boshqarish (While lopps)   -----
+#-- Mentor: Samandar Bozorboyev ---
+
+# while siklining sintaksisi
+# while shartli ifoda:
+#     while siklining kod bloki
+
+# i = 1
+# while i < 6:
+#     print(i)
+#     i += 1
+
+# a = 1
+# while a < 10:
+#     a += 1
+#     print("hello")
+
+## break 
+# i = 1 
+# while i < 900:
+#     if i == 10:
+#         break
+#     i += 1
+#     print("Hello")
+
+
+son = 0
+while son < 6:
+    son += 1
+    if son == 3:
+        continue
+    print(son)
+
